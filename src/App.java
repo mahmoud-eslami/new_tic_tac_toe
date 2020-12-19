17b10105509c5f5a4da55b69730f55c275ca4e0f
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-12-19 13:05:35 +0330

    intial commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        welcomeMethod();
+
+    }
+
+    public static void welcomeMethod() {
+        System.out.println("********************************************");
+        System.out.println("*********** Welcome To XO Game *************");
+        System.out.println("********************************************");
+        System.out.println("********************************************");
+        System.out.println("********************************************");
+        System.out.println("********************************************");
+        System.out.println("********************************************");
+        System.out.println("********************************************");
+    }
+}
