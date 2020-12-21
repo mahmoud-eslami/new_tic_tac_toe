670132eb8ad074f2ea1a6991bfbd74f422b418f2
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-12-21 13:08:57 +0330

    print game mode method added

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        String[][] item = {{"","","",""},{"","","",""},{"","","",""},{"","","",""}};
+
         welcomeMethod();
+        printGameBoard(item);
 
     }
 
@@ -9,10 +12,18 @@ public class App {
         System.out.println("********************************************");
         System.out.println("*********** Welcome To XO Game *************");
         System.out.println("********************************************");
-        System.out.println("********************************************");
-        System.out.println("********************************************");
-        System.out.println("********************************************");
-        System.out.println("********************************************");
-        System.out.println("********************************************");
+    }
+
+    public static void printGameBoard(String[][] gameBoard) {
+        System.out.println("- Game Board :");
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length; j++) {
+                if(gameBoard[i][j] == ""){
+                    gameBoard[i][j] = "-";
+                }
+                System.out.print(gameBoard[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
